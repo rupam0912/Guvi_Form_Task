5a6a1b23620e365a7357a49bff61c773c146caa1
Rupam <rupam@localhost.localdomain>
2020-04-28 10:04:38 -0400

    first commit

---
+++ form.js
@@ -0,0 +1,25 @@
+
+function tableFunc() {
+    var name = document.forms["myForm"]["inputName"].value;
+    var email = document.forms["myForm"]["inputEmail"].value;
+    var age = document.forms["myForm"]["inputAge"].value;
+
+    if(name!="" || email!="" || age!=""){
+        var tableRef = document.getElementById('myTable')
+
+        // Insert a row in the table at the last row
+        var newRow   = tableRef.insertRow(1);
+    
+        // Insert a cell in the row at index 0
+        var newCell1  = newRow.insertCell(0);
+        var newCell2  = newRow.insertCell(1);
+        var newCell3  = newRow.insertCell(2);
+    
+        // Append a text node to the cell
+        newCell1.innerHTML = name;
+        newCell2.innerHTML = email;
+        newCell3.innerHTML = age;
+    }
+   
+   
+}
